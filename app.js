05232963f0534df0b79020a1157cfb221653cf11
Udara Vimukthi <uvlakshan@gmail.com>
2021-11-11 15:47:20 +0530

    Added app.js changes

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('hi');
+
+setTimeout(function(){
+    console.log('after 3 seconds printed');
+},3000); //milisecods
+
+//as a loop
+
+// setInterval(function(){
+//     console.log('Hello udara');
+// },4000);
+
+//loop with time limitation
+var time = 0;
+var timer = setInterval(function(){
+    time += 2;
+    console.log('Hi vim');
+if(time > 5){
+    clearInterval(timer);
+}
+},2000);
+
+console.log(__dirname);
+console.log(__filename);
